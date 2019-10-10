bcaf18fed256d3b0e55348e450d4a3cc2f0299c2
IlyaOrluk <misfits120@mail.ru>
2019-10-11 02:01:36 +0300

    slick header & enhance style

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,12 @@ class CommentsController < ApplicationController
         @comment.save
         redirect_to post_path(@post)
     end
+    def destroy
+        @post = Post.find(params[:id])
+        @comment = @post.comments.find(params[:post_id])
+        @comment.destroy
+        redirect_to post_path(@post)
+    end
 
     private def comment_params
         params.require(:comment).permit(:user_id, :comment)
